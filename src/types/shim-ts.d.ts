a46bc3ff2b765cf45d1a31f30a1a0bc96b8b80da
datlyfe <mrbouaggadmoez@gmail.com>
2018-09-14 23:03:51 +0100

    fix ts typing

--- src/types/shim-ts.d.ts
+++ src/types/shim-ts.d.ts
@@ -1,5 +1,14 @@
+import Vue from "vue";
 declare module "*.jpg" {
   const content: string;
   export default content;
 }
+
+declare module "vue/types/options" {
+  interface ComponentOptions<V extends Vue> {
+    asyncComputed?: any;
+    resource?: string;
+  }
+}
+
 declare const __static: string;
--- src/types/shims-vue.d.ts
+++ src/types/shims-vue.d.ts
@@ -1,12 +1,4 @@
-import Vue from "vue";
-
 declare module "*.vue" {
+  import Vue from "vue";
   export default Vue;
-}
-
-declare module "vue/types/options" {
-  interface ComponentOptions<V extends Vue> {
-    asyncComputed?: any;
-    resource?: string;
-  }
 }
