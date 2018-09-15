bb7e48330922acb523ad1010289d66dea5cc7521
datlyfe <mrbouaggadmoez@gmail.com>
2018-09-15 15:18:08 +0100

    fix build

--- src/types/shim-ts.d.ts
+++ src/types/shim-ts.d.ts
@@ -1,14 +1,6 @@
-import Vue from "vue";
 declare module "*.jpg" {
   const content: string;
   export default content;
-}
-
-declare module "vue/types/options" {
-  interface ComponentOptions<V extends Vue> {
-    asyncComputed?: any;
-    resource?: string;
-  }
 }
 
 declare const __static: string;
